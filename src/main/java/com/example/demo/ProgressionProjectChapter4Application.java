17ebc8882af232ea1fe4d13460755ac0b673e406
loukinoo <lucabagins@gmail.com>
2025-11-15 19:44:42 +0100

    Created project

---
+++ src/main/java/com/example/demo/ProgressionProjectChapter4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressionProjectChapter4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressionProjectChapter4Application.class, args);
+	}
+
+}
