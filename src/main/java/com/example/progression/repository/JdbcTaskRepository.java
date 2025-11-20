890ed75ec9555790b3cdf61a0d1102f1d96233a1
loukinoo <lucabagins@gmail.com>
2025-11-20 11:18:05 +0100

    Implemented class for JdbcTaskRepository

---
+++ src/main/java/com/example/progression/repository/JdbcTaskRepository.java
@@ -0,0 +1,74 @@
+package com.example.progression.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.progression.model.Task;
+
+@Repository
+public class JdbcTaskRepository implements ITaskRepository {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public int save(Task task) {
+		return jdbcTemplate.update("INSERT INTO tasks (task_id, user_id, completed, assigned, \"assignment\")"
+				+ " VALUES (?, ?, ?)", 
+				new Object[] {task.getTaskId(), task.getUserId(), task.isCompleted(), task.isAssigned(), task.getAssignment()});
+		}
+
+	@Override
+	public int update(Task task) {
+		return jdbcTemplate.update("UPDATE tasks SET user_id=?, completed=?, assigned=?, \"assignment\"=? WHERE task_id=?",
+				new Object[] {task.getUserId(), task.isCompleted(), task.isAssigned(), task.getAssignment(), task.getTaskId()});
+	}
+
+	@Override
+	public Task findById(long taskId) {
+		try {
+			Task task = jdbcTemplate.queryForObject("SELECT * FROM tasks WHERE task_id=?", 
+					BeanPropertyRowMapper.newInstance(Task.class), taskId);
+			
+			return task;
+		} catch (IncorrectResultSizeDataAccessException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public int deleteById(long taskId) {
+		return jdbcTemplate.update("DELETE FROM tasks WHERE task_id=?", taskId);
+	}
+
+	@Override
+	public List<Task> findAll() {
+		return jdbcTemplate.query("SELECT * FROM tasks", BeanPropertyRowMapper.newInstance(Task.class));
+	}
+
+	@Override
+	public List<Task> findOfUser(long userId) {
+		return jdbcTemplate.query("SELECT * FROM tasks WHERE user_id=?", BeanPropertyRowMapper.newInstance(Task.class), userId);
+	}
+
+	@Override
+	public List<Task> findCompleted() {
+		return jdbcTemplate.query("SELECT * FROM tasks WHERE completed=true", BeanPropertyRowMapper.newInstance(Task.class));
+	}
+
+	@Override
+	public List<Task> findAssigned() {
+		return jdbcTemplate.query("SELECT * FROM tasks WHERE assigned=true", BeanPropertyRowMapper.newInstance(Task.class));
+	}
+
+	@Override
+	public int deleteAll() {
+		return jdbcTemplate.update("DELETE FROM tasks");
+	}
+
+}
