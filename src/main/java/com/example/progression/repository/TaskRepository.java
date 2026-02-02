bdc94b62146005c6f063847176b338e18f24ee35
loukinoo <lucabagins@gmail.com>
2026-02-02 19:12:31 +0100

    Fixed repositories

--- src/main/java/com/example/progression/repository/TaskRepository.java
+++ src/main/java/com/example/progression/repository/TaskRepository.java
@@ -6,15 +6,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.progression.dto.TaskDTO;
 import com.example.progression.model.Task;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-	int save(TaskDTO task);
-
-	int update(Task task);
+	<S extends Task> S save(S task);
 
 	Optional<Task> findById(Long id);
 
@@ -22,11 +19,11 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 	
 	List<Task> findAll();
 
-	List<Task> findOfUser(Long userId);
+	List<Task> findByUserId(Long userId);
 
-	List<Task> findCompleted(boolean completed);
+	List<Task> findByCompleted(boolean completed);
 	
-	List<Task> findAssigned(boolean assigned);
+	List<Task> findByAssigned(boolean assigned);
 
 	void deleteAll();
 
--- src/main/java/com/example/progression/repository/UserRepository.java
+++ src/main/java/com/example/progression/repository/UserRepository.java
@@ -6,16 +6,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.progression.dto.UserToSaveDTO;
 import com.example.progression.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
-
-	int save(UserToSaveDTO user);
-	
-	int update(User user);
+	<S extends User> S save(S user);
 
 	Optional<User> findById(Long Id);
 
@@ -23,7 +19,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	List<User> findAll();
 
-	List<User> findByRole(boolean admin);
+	List<User> findByAdmin(boolean admin);
 
 	User findByUsername(String username);
 	
